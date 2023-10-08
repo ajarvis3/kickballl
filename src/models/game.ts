7884e47f1584bd3364e0d37e05d02b61fcb9fbde
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2023-10-07 21:46:17 -0400

    Update models

--- src/models/game.ts
+++ src/models/game.ts
@@ -3,39 +3,42 @@ import AtBat from "./atbat";
 import IAtBat from "./types/atbat";
 import IGame from "./types/game";
 
-const GameSchema = new mongoose.Schema<IGame>({
-   _id: {
-      type: String,
+const GameSchema = new mongoose.Schema<IGame>(
+   {
+      _id: {
+         type: String,
+      },
+      ruleTemplateId: {
+         type: String,
+         required: true,
+      },
+      currInning: {
+         type: Number,
+         required: true,
+      },
+      isTopInning: {
+         type: Boolean,
+         required: true,
+      },
+      lineup1Id: {
+         type: String,
+         required: true,
+      },
+      lineup2Id: {
+         type: String,
+         required: true,
+      },
+      date: {
+         type: Date,
+         required: true,
+      },
+      atBatIds: {
+         type: [{ type: AtBat }],
+         required: true,
+      },
    },
-   ruleTemplateId: {
-      type: String,
-      required: true,
-   },
-   currInning: {
-      type: Number,
-      required: true,
-   },
-   isTopInning: {
-      type: Boolean,
-      required: true,
-   },
-   lineup1Id: {
-      type: String,
-      required: true,
-   },
-   lineup2Id: {
-      type: String,
-      required: true,
-   },
-   date: {
-      type: Date,
-      required: true,
-   },
-   atBatIds: {
-      type: [{ type: AtBat }],
-      required: true,
-   },
-});
+   { _id: false }
+);
 
 const Game: mongoose.Model<IGame> = mongoose.model("Game", GameSchema);
 export default Game;
--- src/models/templates.ts
+++ src/models/templates.ts
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 import ILineup from "./types/lineup";
 
-const LineupSchema = new mongoose.Schema<ILineup>({
-   _id: {
-      type: String,
+const LineupSchema = new mongoose.Schema<ILineup>(
+   {
+      _id: {
+         type: String,
+      },
+      teamName: {
+         type: String,
+         required: true,
+      },
+      lineup: {
+         type: [{ type: String }],
+         required: true,
+      },
    },
-   teamName: {
-      type: String,
-      required: true,
-   },
-   lineup: {
-      type: [{ type: String }],
-      required: true,
-   },
-});
+   { _id: false }
+);
 
 const Lineup: mongoose.Model<ILineup> = mongoose.model("Lineup", LineupSchema);
 export default Lineup;
